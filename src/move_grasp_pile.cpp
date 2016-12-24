7b96fb39e3658831ba2b563eb184b4bc3eab614b
Abhijit Makhal <jontromanab.abhijit@gmail.com>
2016-12-24 15:49:40 -0700

    Create move_grasp_pile.cpp
    predefined motion to execute to pick object from pile

---
+++ src/move_grasp_pile.cpp
@@ -0,0 +1,108 @@
+#include <ros/ros.h>
+#include <moveit/move_group_interface/move_group.h>
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "lesson_move_group");
+
+
+  ros::AsyncSpinner spinner(1);
+  spinner.start();
+
+  moveit::planning_interface::MoveGroup group("manipulator");
+  moveit::planning_interface::MoveGroup group2("gripper");
+  group.setPlannerId("RRTkConfigDefault");
+/******************home********************************/
+
+
+  group.setNamedTarget("up");
+  ROS_INFO("Going to UP position");
+  group.move();
+  sleep(1.0);
+/*******************approach********************************/
+
+
+  Eigen::Affine3d approach = Eigen::Translation3d(0.76, -0.42, 1.2)
+                         * Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d::UnitY());
+
+
+  ROS_INFO("Going to Approach position");
+  group.setPoseTarget(approach);
+  group.move() ;
+
+  ROS_INFO("opening gripper");
+  group2.setJointValueTarget("robotiq_85_left_knuckle_joint", 0.01);
+  group2.move();
+
+/**************pick and retreat *****************************/
+
+Eigen::Affine3d pick = approach.translate(0.12*Eigen::Vector3d::UnitX());
+  ROS_INFO("Going to Pick");
+  group.setPoseTarget(pick);
+  group.move();
+
+
+/*******************Closing gripper ************************/
+
+  /*ROS_INFO("Closing gripper");
+  group2.setNamedTarget("close");
+  group2.move();*/
+
+  ROS_INFO("grasping");
+  group2.setJointValueTarget("robotiq_85_left_knuckle_joint", 0.79);
+  group2.move();
+
+
+/**********************Retreat *****************************/
+  Eigen::Affine3d retreat = pick.translate(-0.12*Eigen::Vector3d::UnitX());
+  ROS_INFO("Going to Retreat");
+  group.setPoseTarget(retreat);
+  group.move();
+
+/**********************Place it *****************************/
+  Eigen::Affine3d place = Eigen::Translation3d(-0.133, -0.287, 0.895)
+                         * Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY());
+  ROS_INFO("Going to Place");
+  group.setPoseTarget(place);
+  group.move() ;
+
+/********************Placing ********************************/
+  Eigen::Affine3d placing = place.translate(0.02*Eigen::Vector3d::UnitZ());
+  ROS_INFO("Placing");
+  group.setPoseTarget(placing);
+  group.move();
+
+/*******************Opening gripper ************************/
+
+  //group2.setStartStateToCurrentState ();
+  ROS_INFO("Opening gripper");
+  group2.setJointValueTarget("robotiq_85_left_knuckle_joint", 0.01);
+  group2.move();
+
+/**********************Retreat Again*****************************/
+  Eigen::Affine3d retreat2 = placing.translate(-0.15*Eigen::Vector3d::UnitZ());
+  ROS_INFO("Going to Retreat");
+  group.setPoseTarget(retreat2);
+  group.move();
+
+/*****************Inspect**********************************/
+/*std::map<std::string, double> joints;
+  joints["shoulder_pan_joint"] = 0.1363963462366371;
+  joints["shoulder_lift_joint"] =  0.2109966536389436;
+  joints["elbow_joint"] =  -0.9980383708427638;
+  joints["wrist_1_joint"] =  -2.3544856100677274;
+  joints["wrist_2_joint"] =  -0.13646883251496789;
+  joints["wrist_3_joint"] = -1.4810632157125827;
+  ROS_INFO("Going to Inspect position");
+  group.setJointValueTarget(joints);
+  group.move();
+
+/******************Back to home********************************/
+  group.setNamedTarget("up");
+  ROS_INFO("Going back to UP position");
+  group.move();
+
+  group2.setJointValueTarget("robotiq_85_left_knuckle_joint", 0.8);
+  group2.move();
+
+  sleep(1.0);
+}
